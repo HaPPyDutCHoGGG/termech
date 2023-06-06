a6a3bf1190c4ce77641a4bed522ffc1e9f44449f
HaPPyDutCHoGGG <hdh.egor.shermatov@gmail.com>
2023-06-06 16:02:20 +0300

    normal points on spring

--- dynamic_sys_20.py
+++ dynamic_sys_20.py
@@ -67,17 +67,22 @@ alpha = np.arctan(L/(2*h))
 theta = np.arctan(Ly/Lx) 
 R = h / np.cos(alpha)
 xPs, yPs = Ln*np.cos(theta), Ln*np.sin(theta)
+sP = []
+for i in range(n):
+    item = ax.plot(xE[0] + (i+1)*xPs[0], yE[0] + (i+1)*yPs[0], 'o',color='blue')[0]
+    sP.append(item)
+print(sP)
 
-Ps_0 = ax.plot(xE[0] + xPs[0], yE[0] + yPs[0], 'o',color='blue')[0]
-Ps_1 = ax.plot(xE[0] + 2*xPs[0], yE[0] + 2*yPs[0], 'o',color='blue')[0]
-Ps_2 = ax.plot(xE[0] + 3*xPs[0], yE[0] + 3*yPs[0], 'o',color='blue')[0]
-Ps_3 = ax.plot(xE[0] + 4*xPs[0], yE[0] + 4*yPs[0], 'o',color='blue')[0]
-Ps_4 = ax.plot(xE[0] + 5*xPs[0], yE[0] + 5*yPs[0], 'o',color='blue')[0]
-Ps_5 = ax.plot(xE[0] + 6*xPs[0], yE[0] + 6*yPs[0], 'o',color='blue')[0]
-Ps_6 = ax.plot(xE[0] + 7*xPs[0], yE[0] + 7*yPs[0], 'o',color='blue')[0]
-Ps_7 = ax.plot(xE[0] + 8*xPs[0], yE[0] + 8*yPs[0], 'o',color='blue')[0]
-Ps_8 = ax.plot(xE[0] + 9*xPs[0], yE[0] + 9*yPs[0], 'o',color='blue')[0]
-Ps_9 = ax.plot(xE[0] + 10*xPs[0], yE[0] + 10*yPs[0], 'o',color='blue')[0]
+# Ps_0 = ax.plot(xE[0] + xPs[0], yE[0] + yPs[0], 'o',color='blue')[0]
+# Ps_1 = ax.plot(xE[0] + 2*xPs[0], yE[0] + 2*yPs[0], 'o',color='blue')[0]
+# Ps_2 = ax.plot(xE[0] + 3*xPs[0], yE[0] + 3*yPs[0], 'o',color='blue')[0]
+# Ps_3 = ax.plot(xE[0] + 4*xPs[0], yE[0] + 4*yPs[0], 'o',color='blue')[0]
+# Ps_4 = ax.plot(xE[0] + 5*xPs[0], yE[0] + 5*yPs[0], 'o',color='blue')[0]
+# Ps_5 = ax.plot(xE[0] + 6*xPs[0], yE[0] + 6*yPs[0], 'o',color='blue')[0]
+# Ps_6 = ax.plot(xE[0] + 7*xPs[0], yE[0] + 7*yPs[0], 'o',color='blue')[0]
+# Ps_7 = ax.plot(xE[0] + 8*xPs[0], yE[0] + 8*yPs[0], 'o',color='blue')[0]
+# Ps_8 = ax.plot(xE[0] + 9*xPs[0], yE[0] + 9*yPs[0], 'o',color='blue')[0]
+# Ps_9 = ax.plot(xE[0] + 10*xPs[0], yE[0] + 10*yPs[0], 'o',color='blue')[0]
 
 
 wall_vertical = ax.plot([0, 0], [0, 3], color='blue', linewidth = 5)    
@@ -104,22 +109,25 @@ def kadr(i):
     AB.set_data([xA[i],xB[i]], [yA[i],yB[i]])
     #spring.set_data([(xP[i] + xE[i])*Lx], [(yP[i]*+ yE[i])*Ly])
     spring.set_data([xE[i],xC], [yE[i],yC])
-
-    Ps_0.set_data(xE[i] + xPs[i], yE[i] + yPs[i])
-    Ps_1.set_data(xE[i] + 2*xPs[i], yE[i] + 2*yPs[i])
-    Ps_2.set_data(xE[i] + 3*xPs[i], yE[i] + 3*yPs[i])
-    Ps_3.set_data(xE[i] + 4*xPs[i], yE[i] + 4*yPs[i])
-    Ps_4.set_data(xE[i] + 5*xPs[i], yE[i] + 5*yPs[i])
-    Ps_5.set_data(xE[i] + 6*xPs[i], yE[i] + 6*yPs[i])
-    Ps_6.set_data(xE[i] + 7*xPs[i], yE[i] + 7*yPs[i])
-    Ps_7.set_data(xE[i] + 8*xPs[i], yE[i] + 8*yPs[i])
-    Ps_8.set_data(xE[i] + 9*xPs[i], yE[i] + 9*yPs[i])
-    Ps_9.set_data(xE[i] + 10*xPs[i], yE[i] + 10*yPs[i])
+    
+    for j,item in enumerate(sP):
+        item.set_data(xE[i] + (j+1)*xPs[i], yE[i] + (j+1)*yPs[i])
+    
+    # Ps_0.set_data(xE[i] + xPs[i], yE[i] + yPs[i])
+    # Ps_1.set_data(xE[i] + 2*xPs[i], yE[i] + 2*yPs[i])
+    # Ps_2.set_data(xE[i] + 3*xPs[i], yE[i] + 3*yPs[i])
+    # Ps_3.set_data(xE[i] + 4*xPs[i], yE[i] + 4*yPs[i])
+    # Ps_4.set_data(xE[i] + 5*xPs[i], yE[i] + 5*yPs[i])
+    # Ps_5.set_data(xE[i] + 6*xPs[i], yE[i] + 6*yPs[i])
+    # Ps_6.set_data(xE[i] + 7*xPs[i], yE[i] + 7*yPs[i])
+    # Ps_7.set_data(xE[i] + 8*xPs[i], yE[i] + 8*yPs[i])
+    # Ps_8.set_data(xE[i] + 9*xPs[i], yE[i] + 9*yPs[i])
+    # Ps_9.set_data(xE[i] + 10*xPs[i], yE[i] + 10*yPs[i])
     
 
     
-    return [D, A, E, B, C, DE, AB, spring, 
-            Ps_9,Ps_8,Ps_7,Ps_6,Ps_5,Ps_4,Ps_3,Ps_2,Ps_1,Ps_0]
+    return [D, A, E, B, C, DE, AB, spring, *sP]
+            #Ps_9,Ps_8,Ps_7,Ps_6,Ps_5,Ps_4,Ps_3,Ps_2,Ps_1,Ps_0
 
 kino = FuncAnimation(fig, kadr, interval = t[1]-t[2], frames=len(t))
 
